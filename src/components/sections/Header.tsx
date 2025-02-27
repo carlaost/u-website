c5fe50468c2f1664712a9f8db38c804df2d8cf85
carlaost <ostmanncarla@gmail.com>
2025-02-27 17:53:07 +0100

    fix: login and register referrer links

--- src/components/sections/Header.tsx
+++ src/components/sections/Header.tsx
@@ -6,7 +6,7 @@ export const Header = () => {
     <div className="fixed inset-x-0 top-0 z-10 w-full bg-slate-alpha-1 backdrop-blur-sm">
       <div className="mx-auto flex w-full max-w-screen-xl flex-row items-center justify-between p-4 px-8 ">
         <a href="/">
-          <img src={Logo.src} alt="Logo" className="h-6 w-6" />
+          <img src={Logo.src} alt="Logo" className="size-6" />
         </a>
         <div className="flex flex-row items-center gap-4">
           <a
@@ -18,12 +18,12 @@ export const Header = () => {
           </a>
           <a
             className="text-slate-11 hover:text-slate-10"
-            href="https://app.undermind.ai/?login"
+            href="https://app.undermind.ai/?auth=ogin"
             target="_blank"
           >
             Login
           </a>
-          <a href="https://app.undermind.ai/?register" target="_blank">
+          <a href="https://app.undermind.ai/?auth=register" target="_blank">
             <Button>Sign up</Button>
           </a>
         </div>
